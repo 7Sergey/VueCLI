b1f3ccb50211c5ebd1c799742371b09124a42b80
Sergey Fedorov <sergey14fedorov@gmail.com>
2019-01-21 19:32:35 +0300

    component communication

--- src/Example.vue
+++ src/Example.vue
@@ -1,13 +1,19 @@
 <template> 
     <div>
-     <h1>Пробный текст</h1>
-     <app-input></app-input>
+     <h1>{{ message }}</h1>
+     <app-input :msg="message"></app-input>
     </div>
 </template>
 
 <script>
 import Input from './Field.vue';
+
 export default {
+    data () {
+        return {
+            message: 'Прjjобный текст'
+        }
+    },
     components: {
         'app-input': Input
     }
